035f2a83deaa5cd1855e4ba33f3556b051c199d8
zura12337 <zura.kurta@gmail.com>
2020-12-22 17:40:19 +0400

    📝 Modify Site Settings Controller.

--- web/modules/custom/om_site_settings/src/Controller/OmSiteSettingsController.php
+++ web/modules/custom/om_site_settings/src/Controller/OmSiteSettingsController.php
@@ -14,9 +14,19 @@ class OmSiteSettingsController extends ControllerBase {
    */
   public function build() {
 
+  	$config = \Drupal::config("om_site_settings.site_settings");
+		$archive_length = $config->get("archive_length");
+		$website_code = $config->get("website_code");
+		$website_description = strip_tags($config->get("website_description")['value']);
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->t("
+        <ul>
+        	<li><strong>Archive Length: </strong>$archive_length</li>
+        	<li><strong>Website Code: </strong>$website_code</li>
+        	<li><strong>Website Description: </strong>$website_description</li>
+        </ul>
+      "),
     ];
 
     return $build;
