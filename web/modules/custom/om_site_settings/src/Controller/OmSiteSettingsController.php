9369c49c54f15257d5ea1ad128b0152b8b1041fe
zura12337 <zura.kurta@gmail.com>
2020-12-23 13:40:49 +0400

    🎨 Improve markup in Controller.

--- web/modules/custom/om_site_settings/src/Controller/OmSiteSettingsController.php
+++ web/modules/custom/om_site_settings/src/Controller/OmSiteSettingsController.php
@@ -13,7 +13,6 @@ class OmSiteSettingsController extends ControllerBase {
    * Builds the response.
    */
   public function build() {
-
   	$config = $this->config("om_site_settings.site_settings");
 		$archive_length = $config->get("archive_length");
 		$website_code = $config->get("website_code");
@@ -21,10 +20,14 @@ class OmSiteSettingsController extends ControllerBase {
     $build['content'] = [
       '#type' => 'item',
       '#markup' => $this->t("
-        <h4><strong>Archive Length: </strong>$archive_length</h4>
-        <h4><strong>Website Code: </strong>$website_code</h4>
-        <h4><strong>Website Description: </strong>$website_description</h4>
-      "),
+        <h4><strong>Archive Length: </strong>@archive_length</h4>
+        <h4><strong>Website Code: </strong>@website_code</h4>
+        <h4><strong>Website Description: </strong>@website_description</h4>
+      ", [
+      	'@archive_length' => $archive_length,
+	      '@website_code' => $website_code,
+	      '@website_description' => $website_description
+      ]),
     ];
 
     return $build;
